5258d665d7526ec3a1b2860fc13e2fe1fcfcd79e
Mohamad Chebli <mohamed.chebli@lau.edu>
2023-02-02 21:58:44 +0200

    fixes

---
+++ scripts/movieDetailService.js
@@ -0,0 +1,21 @@
+/// <reference path="script.js" />
+movieApp.factory("detailService", function ($http, $log) {
+  return {
+    getDetails: function (movie_id) {
+      $http({
+        method: "GET",
+        url:
+          "https://api.themoviedb.org/3/movie/" +
+          movie_id +
+          "?api_key=4f35139a7aeecfe122ffd50f642cd92b",
+      })
+        .then(function (movieDetailResponse) {
+          //   console.log("clicked movie id:" + movie_id);
+          //   console.log(movieDetails);
+          $log.info(movieDetailResponse.data);
+          return movieDetailResponse.data;
+        })
+        .catch(console.error);
+    },
+  };
+});
